3ed2735672444307abbaaf5b83108507a886429a
Erik Hollensbe <github@hollensbe.org>
2020-10-09 05:38:32 -0700

    Move unify routine to merge.go
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- both.go
+++ both.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"strings"
 	"time"
 
 	"google.golang.org/api/calendar/v3"
@@ -58,72 +56,4 @@ func taskTimeToGCal(tw taskWarriorTime) (*calendar.EventDateTime, error) {
 	}
 
 	return &calendar.EventDateTime{DateTime: toCalendarTime(t), TimeZone: time.Local.String()}, nil
-}
-
-func unify(task *taskWarriorItem, event *calendar.Event) (bool, error) {
-	if task == nil || event == nil {
-		return false, errors.New("nil event or task passed")
-	}
-
-	modified := false
-	taskNewer := true
-
-	taskModified, err := task.Modified.ToTime()
-	if err != nil {
-		taskModified = time.Time{}
-	}
-
-	var calModified time.Time
-
-	ct, err := gcalTimeToTaskWarrior(event.Updated)
-	if err != nil {
-		calModified = time.Time{}
-	} else {
-		calModified, err = ct.ToTime()
-		if err != nil {
-			calModified = time.Time{}
-		}
-	}
-
-	if taskModified.Before(calModified) {
-		taskNewer = false
-	}
-
-	if task.CalendarID != event.Id {
-		task.CalendarID = event.Id
-		modified = true
-	}
-
-	if strings.TrimSpace(task.Description) != strings.TrimSpace(event.Summary) {
-		if taskNewer {
-			event.Summary = strings.TrimSpace(task.Description)
-			task.Description = event.Summary
-		} else {
-			task.Description = strings.TrimSpace(event.Summary)
-			event.Summary = task.Description
-		}
-		modified = true
-	}
-
-	due, err := eventDue(event)
-	if err != nil {
-		return false, err
-	}
-
-	if due != task.Due {
-		if taskNewer {
-			var err error
-			event.Start, err = taskTimeToGCal(task.Due)
-			event.End = event.Start
-			if err != nil {
-				return false, fmt.Errorf("Task %q could not assign start time to calendar id %q: %w", task.UUID, event.Id, err)
-			}
-		} else {
-			task.Due = due
-		}
-
-		modified = true
-	}
-
-	return modified, nil
 }
